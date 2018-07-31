dee3c7bc6394ee182ea30f7fc47a78998cc902ac
Mike <lanselot777hailander@gmail.com>
2018-07-31 14:53:30 -0400

    implement edit and delete

--- myrecipes/test/integration/recipes_test.rb
+++ myrecipes/test/integration/recipes_test.rb
@@ -27,6 +27,8 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
     assert_match @chef.chefname, response.body
+    assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
   end
 
   test "create new valid recipe" do
@@ -39,7 +41,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_match name_of_recipe.capitalize, response.body
-    assert_match description_of_recipe, response.body    
+    assert_match description_of_recipe, response.body
   end
 
   test "reject invalid recipe submissions" do
