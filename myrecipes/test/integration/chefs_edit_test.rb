a9206a18edbd6e9b10471354f6e574078fca2f98
Mike <lanselot777hailander@gmail.com>
2018-08-13 15:14:19 -0400

    Complete Authentication System with Admin and tests

--- myrecipes/test/integration/chefs_edit_test.rb
+++ myrecipes/test/integration/chefs_edit_test.rb
@@ -4,6 +4,8 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
 
   def setup
     @chef = Chef.create!(chefname: "mashur", email: "mashur@example.com", password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "John", email: "john@example.com", password: "password", password_confirmation: "password")
+    @admin_user = Chef.create!(chefname: "John", email: "john1@example.com", password: "password", password_confirmation: "password", admin: true)
   end
 
   test "reject an invalid edit" do
@@ -16,7 +18,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.panel-body'
   end
 
-  test "accept valid signup" do
+  test "accept valid edit" do
     sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
@@ -26,6 +28,30 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     @chef.reload
     assert_match "Mashur", @chef.chefname
     assert_match "mashur@exapl.com", @chef.email
+  end
+
+  test "accept edit attempt by admin user" do
+    sign_in_as(@admin_user, "password")
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: "Mashur1", email: "mashur1@exapl.com"} }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "Mashur1", @chef.chefname
+    assert_match "mashur1@exapl.com", @chef.email
+  end
+
+  test "redirect edit attempt by another non-admin user" do
+    sign_in_as(@chef2, "password")
+    updated_name = "joe"
+    updated_email = "joe@example.com"
+    patch chef_path(@chef), params: { chef: { chefname: updated_email, email: updated_email} }
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "mashur", @chef.chefname
+    assert_match "mashur@example.com", @chef.email
   end
 
 end
