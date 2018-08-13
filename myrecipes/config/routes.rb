0e9be40105e7ed6d3869bd732124e785fbe64158
Mike <lanselot777hailander@gmail.com>
2018-08-13 11:49:08 -0400

    System of authenticacion with test created

--- myrecipes/config/routes.rb
+++ myrecipes/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get "pages/home", to: 'pages#home'
 
-  #resources
+  #resources of recipe
   get '/recipes', to: 'recipes#index'
   get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
   get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
@@ -11,7 +11,12 @@ Rails.application.routes.draw do
   get '/recipes/:id', to: 'recipes#show', as: 'recipe'
   delete '/recipes/:id', to: 'recipes#destroy'
   post '/recipes', to: 'recipes#create'
+  #resources of recipe
 
   get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
