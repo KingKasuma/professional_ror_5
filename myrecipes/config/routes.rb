3f81c5c6358421c27a2c6196355436d0f146ab99
Mike <lanselot777hailander@gmail.com>
2018-08-21 16:54:34 -0400

    Complete comments feature

--- myrecipes/config/routes.rb
+++ myrecipes/config/routes.rb
@@ -3,15 +3,19 @@ Rails.application.routes.draw do
   get "pages/home", to: 'pages#home'
 
   #resources of recipe
-  get '/recipes', to: 'recipes#index'
-  get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
-  get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
-  patch '/recipes/:id', to: 'recipes#update'
-  put '/recipes/:id', to: 'recipes#update'
-  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
-  delete '/recipes/:id', to: 'recipes#destroy'
-  post '/recipes', to: 'recipes#create'
+  # get '/recipes', to: 'recipes#index'
+  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  # get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  # patch '/recipes/:id', to: 'recipes#update'
+  # put '/recipes/:id', to: 'recipes#update'
+  # get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  # delete '/recipes/:id', to: 'recipes#destroy'
+  # post '/recipes', to: 'recipes#create'
   #resources of recipe
+
+  resources :recipes do
+    resources :comments, only: [:create]
+  end
 
   get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
